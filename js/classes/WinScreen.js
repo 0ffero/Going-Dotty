4b9eba8c83a01d9b0d092b7f3b9c50a71a9a3109
0ffero <41750813+0ffero@users.noreply.github.com>
2022-10-30 20:38:48 +0000

    WinScreen Update

--- js/classes/WinScreen.js
+++ js/classes/WinScreen.js
@@ -28,16 +28,73 @@ let WinScreen = class {
 
         let font = { ...vars.fonts.default };
         let name = 'AAA'; let points = 0;
-        let text = this.phaserObjects.winText = scene.add.text(cC.cX, this.positionsY.text, `${name} wins with ${points}`,font).setOrigin(0.5);
+        let text = this.phaserObjects.winText = scene.add.text(cC.cX, this.positionsY.text, `${name} wins with ${points}`,font).setOrigin(0.5).setAlpha(0);
+        text.show = (_show=true)=> {
+            let alpha=_show?1:0;
+            scene.tweens.add({
+                targets: text, alpha: alpha, duration: 1000,
+                onComplete: ()=> {
+                    _show ? vars.game.winScreen.showContinueButton(true) : vars.game.winScreen.showContinueButton(false);
+                }
+            });
+        }
         this.container.add(text);
+
+        let cCon = this.continueContainer = scene.add.container().setName('winScreen_continue');
+        cCon.show  = (_show=true)=> {
+            if (cCon.tween) { cCon.tween.remove(); cCon.tween=null };
+            let alpha=_show?1:0;
+            let duration = !_show ? 100 : 2000;
+            cCon.tween = scene.tweens.add({ targets: cCon, alpha: alpha, duration: duration, onComplete: ()=> { cCon.tween=null} });
+        }
+        
+        let continueButton = scene.add.image(cC.cX,cC.height*0.8, 'newGameButtonBG').setInteractive();
+        continueButton.on('pointerup', ()=> {
+            // we need to show the options screen
+            debugger;
+        });
+        let continueText = scene.add.text(cC.cX,cC.height*0.8, 'CONTINUE', font).setOrigin(0.5);
+        this.continueContainer.add([continueButton,continueText]);
+        this.container.add(this.continueContainer);
         this.show(false);
+    }
+
+    fadeInWinText() {
+        this.phaserObjects.winText.show();
+    }
+
+    generateWinVars() {
+        debugger;
+        this.scoreSets.forEach((_sS,_i)=> {
+            let winPosition = _i;
+            // for each of the scores add the finishing y position based on the players winning position
+            switch (winPosition) {
+                case 0: // winning position
+                    sS.y = this.positionsY.win;
+                    // generate the win text
+                    debugger;
+                break;
+
+                case 1: // 2nd position
+                    sS.y = this.positionsY.secondPlace;
+                break;
+
+                case 2: // 3rd position
+                    sS.y = this.positionsY.thirdPlace;
+                break;
+
+                case 3: // 4th position
+                    sS.y = this.positionsY.fourthPlace;
+                break;
+            };
+
+        });
     }
 
     orderPlayerScores() {
         this.scoreSets = [];
         for (let pID in this.playerScores) {
             let score = this.playerScores[pID];
-            //!this.scoreSets[score] && (this.scoreSets[score] = []);
             let index = this.scoreSets.findIndex(m=>m.score===score);
             index===-1 ? this.scoreSets.push({ score: score, pID: [pID]}) : this.scoreSets[index].pID.push(pID);
         };
@@ -88,11 +145,6 @@ let WinScreen = class {
     }
 
     show(_show=true, _order=[]) {
-        console.error(`TODO: START FROM HERE ->\n\
-                        We can request a show by playing a few moves so the players have points\n\
-                        we need to update the win text using this.updateWinText()\n\
-                        before continuing!`);
-        debugger;
         let min = 2;
         let max = vars.game.options.playersMax;
         if (_show && (!checkType(_order,'array') || _order.length<min || _order.length>max)) return `Invalid order sent, it should look something like ['p3','p1','p2','p4'] and must be at LEAST 2 in length`;
@@ -100,11 +152,25 @@ let WinScreen = class {
         this.container.setVisible(_show);
         if (!_show) { this.resetPlayerDots(); return; };
 
+        console.error(`TODO: START FROM HERE ->\n\
+        We can request a show by playing a few moves so the players have points\n\
+        we need to update the win text using this.updateWinText()\n\
+        before continuing!`);
+        debugger;
+        let wV = this.generateWinVars();
+
+        this.updateWinText(wV.name,wV.points);
+
         this.repositionDotsByPoints(_order);
+    }
+
+    showContinueButton(_show=true) {
+        this.continueContainer.show(_show);
     }
 
     // incoming _name can be a single player eg 'SJF', or it can be multiple players eg 'SJF and ABC'
     updateWinText(_name, _points) {
         this.phaserObjects.winText.text = `${_name} wins with ${_points}`;
+        this.fadeInWinText();
     }
 };
